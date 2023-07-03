dd176329335073dd030ccdbf551c618be76a00ce
ashu <vermaprerita@gmail.com>
2023-07-04 02:22:10 +0530

    register user

--- app.js
+++ app.js
@@ -23,6 +23,37 @@ app.use((err, req, res, next) => {
 
 // Routes
 
+// Register a new user
+app.post(
+  "/register",
+  body("username").notEmpty().withMessage("Username is required"),
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const { username, password } = req.body;
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      const user = new User({
+        username,
+        password: hashedPassword,
+      });
+      await user.save();
+      res.status(201).json({ message: "User registered successfully" });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "Server Error" });
+    }
+  }
+);
+
 app.listen(PORT, () => {
   console.log(`Server start on port : ${PORT}`);
 });
